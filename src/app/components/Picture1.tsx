241db60e881f22d024090822b04e0cc4aad06e03
ValdryanIvandito <valdryan08@gmail.com>
2024-03-02 20:41:46 +0700

    fixed

--- src/app/components/Picture1.tsx
+++ src/app/components/Picture1.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Picture1 = () => {
   return (
     <Image
-      src="/nusa-finance-Token.png"
+      src="/nusa-finance-token.png"
       width={700}
       height={500}
       alt="Nusa Finance Token"
